03bfec6188142dcfdf17e88ffa7a4bc24c001e1c
codepandaLN <2637248917@qq.com>
2023-03-17 20:25:45 +0800

    用户中心管理系统

---
+++ user-center-backend/sql/ddl.sql
@@ -0,0 +1,23 @@
+-- 创建库
+create database leave;
+
+-- 切换库
+use leave;
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    userName     varchar(256)                           null comment '用户昵称',
+    userAccount  varchar(256)                           not null comment '账号',
+    userAvatar   varchar(1024)                          null comment '用户头像',
+    gender       tinyint                                null comment '性别（0-男, 1-女）',
+    userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
+    userPassword varchar(512)                           not null comment '密码',
+    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除',
+    constraint uni_userAccount
+        unique (userAccount)
+) comment '用户';
+
---
+++ user-center-backend/src/main/java/com/panda/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.panda.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.panda.model.entity.User;
+
+/**
+ * @Entity com.panda.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
